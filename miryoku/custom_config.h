83fb9cbbe91f459e9a1ea409716f7627c38c5850
jococo1221 <62574963+jococo1221@users.noreply.github.com>
2022-10-23 22:35:11 -0400

    attempt to add custom options to custom_config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,9 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+//mouse keys
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
+//layers
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_TAP_QWERTY
